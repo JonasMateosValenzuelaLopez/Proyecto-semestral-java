4620dfb9356ef41b772b24d7fba057835da34a06
Jonas <jon.valenzuelal@duocuc.cl>
2026-04-28 18:49:05 -0400

    feature: carpetas microservicios

---
+++ ms-coindencias/src/main/java/com/sanosysalvos/arqui_proyecto/ArquiProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.arqui_proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiProyectoApplication.class, args);
+	}
+
+}
---
+++ ms-integraciones/src/test/java/com/sanosysalvos/arqui_proyecto/ArquiProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.arqui_proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
